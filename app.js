9fca05bc80ed6a3a0dfea5866cd63b41e31aae11
Iulian Rusu <iulian.rusu2@student.tuiasi.ro>
2021-05-11 13:15:45 +0300

    Add user management with sessions (Lab 11 Part 2,3)

--- app.js
+++ app.js
@@ -1,43 +1,80 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const favicon = require('serve-favicon')
 const path = require('path')
+const session = require('express-session')
 const utility = require("./modules/utility");
 
 const app = express();
-
+ 
 app.set('view engine', 'ejs');
 
 app.use(expressLayouts);
 app.use(express.static('public'))
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')))
+app.use(session({
+  secret: '$up3r-$3cr3t',
+  resave: true,
+  saveUninitialized: true
+}));
 
-let listaIntrebari;
+let listaIntrebari, listaUtilizatori;
 (async () => { listaIntrebari = await utility.asyncReadFile("intrebari.json"); })();
+(async () => { listaUtilizatori = await utility.asyncReadFile("utilizatori.json"); })();
 
-app.get('/', (req, res) => res.render("index", {
+app.get('/', (req, res) => {
+	res.render("index", {
+	session: req.session,
 	title: "Toasted",
-	styleList: ["index-style.css"]}));
+	styleList: ["index-style.css"]});
+});
 app.get('/chestionar', (req, res) => res.render('chestionar', { 
 	title: "Chestionar",
+	session: req.session,
 	styleList: ["quiz-style.css"],
 	intrebari: listaIntrebari
  }));
-app.get('/autentificare', (req, res) => res.render("autentificare", {
-	title: "Autentificare",
-	styleList: ["auth-style.css"],
-}));
+app.get('/autentificare', (req, res) => {
+	res.render("autentificare", {
+		error: req.cookies['error'],
+		title: "Autentificare",
+		session: req.session,
+		styleList: ["auth-style.css"],
+	});
+});
 app.post('/rezultat-chestionar', (req, res) => res.render('rezultat-chestionar', {
 	title: "Rezultate",
+	session: req.session,
 	styleList: ["quiz-style.css"],
 	intrebari: listaIntrebari, 
 	raspunsuri: JSON.stringify(req.body) }));
 app.post('/verificare-autentificare', (req, res) => {
-	res.redirect("/");
+	const username = req.body["username"];
+	const password = req.body["password"];
+	for(const u of listaUtilizatori) {
+		if (u.username == username && u.password == password) {
+			req.session.user = {
+				username: u.username,
+				email: u.email,
+				phone: u.phone,
+				firstName: u.firstName,
+				lastName: u.lastName
+			};
+			res.redirect("/");
+			return;
+		}
+	}
+	res.cookie('error', 'Date invalide', {maxAge: 1000});
+	res.redirect("/autentificare");
 });
-
+app.get('/logout', (req, res) => {
+	req.session.user = null;
+	res.redirect('/');
+});
 const port = 6789;
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost: ${port}`));
